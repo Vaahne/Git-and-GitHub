96d3fb3192ba63231520d9ccd08e7f2cfb1ee4fb
Spring -Git <vaahne@yahoo.com>
2025-07-28 20:32:25 -0600

    commented disk full undefined lines

--- practice/os1.py
+++ practice/os1.py
@@ -9,9 +9,9 @@ def main():
     if check_reboot():
         print ("Pending Reboot!!")
         sys.exit(1)
-    if disk_ful():
-        print("Disk full")
-        sys.exit(1)
+    # if disk_ful():
+    #     print("Disk full")
+    #     sys.exit(1)
     print ("Everything ok!!")
     sys.exit(0)
     # pass    # pass means do something later, do nothing now 
