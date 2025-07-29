88e74a843216bc2d7282fcabaed6ac3852c54e69
Spring -Git <vaahne@yahoo.com>
2025-07-28 20:28:20 -0600

    added l

--- practice/os1.py
+++ practice/os1.py
@@ -9,7 +9,7 @@ def main():
     if check_reboot():
         print ("Pending Reboot!!")
         sys.exit(1)
-    if disk_ful():
+    if disk_full():
         print("Disk full")
         sys.exit(1)
     print ("Everything ok!!")
