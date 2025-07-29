2c356063f6bbb42d3f0874fd783574aa2cc5ac06
Spring -Git <vaahne@yahoo.com>
2025-07-28 19:58:31 -0600

    removed os.py

--- practice/os.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-def check_reboot():
-        """Reboot True if the computer has a pending reboot."""
-        return os.path.exists("/run/reboot-required")
-def main():
-    pass    # pass means do something later, do nothing now 
-
-main()
