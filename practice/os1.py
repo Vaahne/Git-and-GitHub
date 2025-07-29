aefc08690c5d2f0ca0a335516777819f0fd52dae
Spring -Git <vaahne@yahoo.com>
2025-07-28 20:27:16 -0600

    add disk_full call check

--- practice/os1.py
+++ practice/os1.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 import os
+import sys
+
 def check_reboot():
         """Reboot True if the computer has a pending reboot."""
         return os.path.exists("/run/reboot-required")
 def main():
-    pass    # pass means do something later, do nothing now 
+    if check_reboot():
+        print ("Pending Reboot!!")
+        sys.exit(1)
+    if disk_ful():
+        print("Disk full")
+        sys.exit(1)
+    print ("Everything ok!!")
+    sys.exit(0)
+    # pass    # pass means do something later, do nothing now 
 
 main()
